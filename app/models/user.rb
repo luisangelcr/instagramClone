f2047f662a0942f24ab3aca3804ed1fab88363a2
luisangelcr <luisangelcr80@outlook.com>
2023-09-19 22:26:34 +0300

    added FollowController, fixing follow errors, added follow and unfollow requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :posts, dependent: :destroy
   has_one_attached :profile_pic
 
@@ -10,15 +9,16 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
-  has_many :follow_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
+  has_many :follow_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "followed_id"
 
   has_many :accepted_recieved_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "followed_id"
 
-  has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "followed_id"
+  has_many :accepted_sent_requests, -> { where(accepted: true) }, class_name: "Follow", foreign_key: "follower_id"
 
   # has_many :recieved_requests, class_name: "Follow", foreign_key: "followed_id"
   # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
-  # has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
+  has_many :waiting_sent_requests, -> { where(accepted: false) }, class_name: "Follow", foreign_key: "follower_id"
+
   has_many :followers, through: :accepted_recieved_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
 
@@ -29,5 +29,9 @@ class User < ApplicationRecord
 
   def unfollow(user)
     self.accepted_sent_requests.find_by(followed: user)&.destroy
+  end
+
+  def cancel_request(user)
+    self.waiting_sent_requests.find_by(followed: user)&.destroy
   end
 end
