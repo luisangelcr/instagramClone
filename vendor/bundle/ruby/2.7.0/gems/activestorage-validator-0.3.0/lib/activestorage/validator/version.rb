f8496d58ab25fe73395ab595b5a0af60ec687ff8
luisangelcr <luisangelcr80@outlook.com>
2024-04-22 10:59:11 +0300

    update readme

--- vendor/bundle/ruby/2.7.0/gems/activestorage-validator-0.3.0/lib/activestorage/validator/version.rb
+++
@@ -1,5 +0,0 @@
-module ActiveStorage
-  module Validator
-    VERSION = '0.3.0'
-  end
-end
--- vendor/bundle/ruby/2.7.0/gems/bootstrap-5.3.0.alpha3/lib/bootstrap/version.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Bootstrap
-  VERSION       = '5.3.0.alpha3'
-  BOOTSTRAP_SHA = 'ac576614a5515e429f27e756fad81d5aa05e95a6'
-end
